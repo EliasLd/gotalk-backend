15f7417bb6a4f5e6803e5cd5a5138a73f0cc2974
Eliasld <area_causation872@simplelogin.fr>
2025-06-09 12:31:47 +0200

    Add test for registration of an already existing user

--- internal/service/user_service_test.go
+++ internal/service/user_service_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/EliasLd/gotalk-backend/internal/repository"
+	"github.com/EliasLd/gotalk-backend/internal/service/errors"
 )
 
 // Test object used to safely access user repository
@@ -42,4 +43,26 @@ func TestRegisterUser_Success(t *testing.T) {
 
 	defer repository.CleanUpUser(t, user.ID, s.repo)
 
+}
+
+func TestRegisterUser_UsernameAlreadyExists(t *testing.T) {
+	s := setupService(t)
+
+	username := "testuser_taken"
+	password := "AnotherValid123!"
+
+	existingUser, err := s.RegisterUser(context.Background(), username, password)
+	if err != nil {
+		t.Fatalf("Expected no error during user registration, got %v", err)
+	}
+
+	defer repository.CleanUpUser(t, existingUser.ID, s.repo)
+
+	newUser, err := s.RegisterUser(context.Background(), username, password)
+	if err != errors.ErrUserAlreadyExists {
+		t.Fatalf("expected ErrUserAlreadyExists, got %v", err)	
+	}
+	if newUser != nil {
+		t.Errorf("Expected no user, got %v", newUser)
+	}
 }

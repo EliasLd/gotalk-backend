66dfc84ae2f646184348da2123afab9c14a134c7
Eliasld <area_causation872@simplelogin.fr>
2025-06-15 15:58:55 +0200

    Test GenerateToken and ValidateToken function happy case

---
+++ internal/auth/jwt_test.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"testing"
+
+	"github.com/EliasLd/gotalk-backend/internal/repository"
+)
+
+func TestGenerateTokenAndValidateToken_Succes(t *testing.T) {
+	user := repository.NewTestUser(t, "testuser")
+
+	token, err := GenerateToken(user)
+	if err != nil {
+		t.Fatalf("Unexpected error generating token: %v", err)
+	}
+
+	claims, err := ValidateToken(token)
+	if err != nil {
+		t.Fatalf("Unexpected error validating token: %v", err)
+	}
+
+	if claims.UserID != user.ID {
+		t.Errorf("Expected userID %v, got %v", user.ID, claims.UserID)
+	}
+}
+

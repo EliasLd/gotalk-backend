1f5cc8b29c573f7cac747b0de762d9a65cdf8c11
Eliasld <area_causation872@simplelogin.fr>
2025-06-16 19:31:24 +0200

    Test expired token validation

--- internal/auth/jwt_test.go
+++ internal/auth/jwt_test.go
@@ -2,8 +2,11 @@ package auth
 
 import (
 	"testing"
+	"time"
 
 	"github.com/EliasLd/gotalk-backend/internal/repository"
+	"github.com/google/uuid"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func TestGenerateTokenAndValidateToken_Succes(t *testing.T) {
@@ -30,5 +33,25 @@ func TestValidateToken_InvalidToken(t *testing.T) {
 	_, err := ValidateToken(invalidToken)
 	if err == nil {
 		t.Fatal("Expected error for invalid token, got nil")
+	}
+}
+
+func TestValidateToken_ExpiredToken(t *testing.T) {
+	userID := uuid.New()
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, &Claims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(-1 * time.Hour)),
+		},
+	}).SignedString(jwtSecret)
+
+	if err != nil {
+		t.Fatalf("failed to generate expired token: %v", err)
+	}
+
+	_, err = ValidateToken(token)
+	if err == nil {
+		t.Fatal("expected error for expired token, got nil")
 	}
 }

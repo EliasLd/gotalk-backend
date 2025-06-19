9899e163cf40a075c1d9f9913cfd34beeb4bac38
Eliasld <area_causation872@simplelogin.fr>
2025-06-19 19:44:51 +0200

    Add AuthMiddleware function

---
+++ internal/http/middleware/auth.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"context"
+	"strings"
+	"net/http"
+
+	"github.com/EliasLd/gotalk-backend/internal/auth"
+)
+
+const userIDKey string = "userID"
+
+// Middleware that checks JWT in authorization header
+func AuthMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "Missing or invalid Authorization header", http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+
+		userID, err := auth.ValidateToken(tokenStr)
+		if err != nil {
+			http.Error(w, "Invalid or expired token", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), userIDKey, userID)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}

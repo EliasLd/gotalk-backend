2e6e2e4a3550141863b477f215bb59762bcceae4
Eliasld <area_causation872@simplelogin.fr>
2025-06-22 11:05:10 +0200

    Add router file to improve unicity

---
+++ internal/http/router.go
@@ -0,0 +1,20 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/EliasLd/gotalk-backend/internal/handlers"
+	"github.com/EliasLd/gotalk-backend/internal/http/middleware"
+)
+
+func NewRouter(handler * handlers.Handler) http.Handler {
+	mux := http.NewServeMux()
+
+	// Public routes
+	mux.HandleFunc("/health", handlers.HealthHandler)
+
+	// Private routes
+	mux.Handle("/me", middleware.AuthMiddleware(http.HandlerFunc(handler.HandleGetMe)))
+
+	return mux
+}
